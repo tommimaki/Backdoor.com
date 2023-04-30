609fce82552eb53e4329720492c288cb35ff6154
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-01 02:47:24 +0300

    header fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default function Header({ isVisible }) {
     >
       <div className="flex justify-between">
         <Link href="/">
-          <div className="flex flex-col text-amber-950 items-center text-lg justify-center pl-20 pt-4">
+          <div className="flex flex-col p-3 text-amber-950 items-center text-lg justify-center ml-20 mt-4  rounded-full border-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -26,25 +26,29 @@ export default function Header({ isVisible }) {
                 d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
               />
             </svg>
-            <h2 className="">Backdoor</h2>
+            <h2 className="">
+              <b>Backdoor</b>
+            </h2>
           </div>
         </Link>
 
-        <div className="pr-20 pt-4 text-amber-950">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-10 h-10"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-            />
-          </svg>
+        <div className=" mr-20 p-5 mt-4 inline-flex items-center text-amber-950 rounded-full border-2 ">
+          <Link href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-12 h-12"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+          </Link>
         </div>
       </div>
     </header>
