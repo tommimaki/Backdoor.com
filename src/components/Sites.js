388457e2eb7264289b4b2b616c449cd287830fa9
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-01 12:01:31 +0300

    current rehaul

--- src/components/Sites.js
+++ src/components/Sites.js
@@ -17,95 +17,88 @@ export default function Sites() {
             <h1 className="text-5xl text-amber-950 text-center font-bold mb-8">
               Our Current Projects
             </h1>
-            <a
+            <div
               class="block rounded-xl border border-gray-800 bg-green-700 mb-2 p-4 shadow-xl sm:p-6 lg:p-8"
               href=""
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10 text-blue-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                />
-              </svg>
-
-              <h3 class="mt-3 text-lg font-bold text-white sm:text-xl">
-                Lorem, ipsum dolor.
-              </h3>
+              <div className="flex justify-between">
+                <div className="flex items-center gap-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    class="h-10 w-10 text-blue-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+                    />
+                  </svg>
+                  <h3 class="mt-3 text-lg flex font-bold text-white sm:text-xl">
+                    Kallio Revival
+                  </h3>
+                </div>
+                <button className="greenButton ">View</button>
+              </div>
 
               <p class="mt-4 text-md text-gray-100">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
-                eius labore nisi tempore modi vel voluptate ullam nostrum
-                adipisci suscipit eaque quae cupiditate, accusamus minus
-                laboriosam totam laborum, deserunt sint.
+                We are proud to be working on several exciting construction
+                projects in the Helsinki and Uusimaa region. One of our most
+                prominent projects is the development of a modern residential
+                complex in the heart of Helsinki. This project, named "Kotka
+                Towers," features energy-efficient apartments with a range of
+                smart home technologies and green spaces for residents to enjoy.
               </p>
-            </a>
-            <a
-              class="block rounded-xl border border-gray-800 bg-green-700 mb-2 p-4 shadow-xl sm:p-6 lg:p-8"
+            </div>
+            <div class="block rounded-xl border border-gray-800 bg-green-700 mb-2 p-4 shadow-xl sm:p-6 lg:p-8">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    class="h-10 w-10 text-blue-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                    />
+                  </svg>
+                  <h3 class="mt-3 text-lg flex font-bold text-white sm:text-xl">
+                    Kotka towers
+                  </h3>
+                </div>
+                <button className="greenButton ">View</button>
+              </div>
+
+              <p class="mt-4 text-md text-gray-100">
+                In addition to Kotka Towers, we are also working on the
+                renovation of a historic building in the bustling district of
+                Kallio. Our team is dedicated to preserving the architectural
+                integrity of the building while updating its interiors to meet
+                the needs of modern urban living. This project, dubbed "Kallio
+                Revival," will provide a mix of residential and commercial
+                spaces, including trendy cafes, artisan shops, and co-working
+                facilities.
+              </p>
+            </div>
+            <div
+              class="block rounded-xl border border-gray-800 bg-green-700 mb-2 p-4 shadow-xl sm:p-2 lg:p-4"
               href=""
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10 text-blue-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                />
-              </svg>
-
-              <h3 class="mt-3 text-lg font-bold text-white sm:text-xl">
-                Lorem, ipsum dolor.
-              </h3>
-
-              <p class="mt-4 text-md text-gray-100">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
-                eius labore nisi tempore modi vel voluptate ullam nostrum
-                adipisci suscipit eaque quae cupiditate, accusamus minus
-                laboriosam totam laborum, deserunt sint.
+              <p class="text-md text-gray-100">
+                We are excited to contribute to the growth and development of
+                the Helsinki and Uusimaa region through these projects, and we
+                look forward to unveiling more innovative construction projects
+                in the near future.
               </p>
-            </a>
-
-            <p className="mb-4 border-2  border-green-800 p-4 text-lg ">
-              We are proud to be working on several exciting construction
-              projects in the Helsinki and Uusimaa region. One of our most
-              prominent projects is the development of a modern residential
-              complex in the heart of Helsinki. This project, named "Kotka
-              Towers," features energy-efficient apartments with a range of
-              smart home technologies and green spaces for residents to enjoy.
-            </p>
-            <p className="mb-4 border-2 border-green-800 p-4 text-lg ">
-              In addition to Kotka Towers, we are also working on the renovation
-              of a historic building in the bustling district of Kallio. Our
-              team is dedicated to preserving the architectural integrity of the
-              building while updating its interiors to meet the needs of modern
-              urban living. This project, dubbed "Kallio Revival," will provide
-              a mix of residential and commercial spaces, including trendy
-              cafes, artisan shops, and co-working facilities.
-            </p>
-
-            <p className="border-2 p-4 border-green-800 text-lg">
-              We are excited to contribute to the growth and development of the
-              Helsinki and Uusimaa region through these projects, and we look
-              forward to unveiling more innovative construction projects in the
-              near future.
-            </p>
+            </div>
             <button className="greenButton mt-10 w-full">See Them All</button>
           </div>
         </div>
