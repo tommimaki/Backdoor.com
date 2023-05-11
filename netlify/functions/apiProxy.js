2a37e43c5d35ceae3e27fc186d50fb3327f02a8c
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-11 17:19:47 +0200

    function fix

--- netlify/functions/apiProxy.js
+++ netlify/functions/apiProxy.js
@@ -3,11 +3,12 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   const { path } = event;
 
+  path = path.replace("/.netlify/functions/apiProxy", "");
+
   try {
-    console.log(path);
     const response = await axios({
       method: "GET",
-      url: `http://16.170.141.178:3001/api${path}`,
+      url: `http://16.170.141.178:3001/api/${path}`,
     });
 
     return {
