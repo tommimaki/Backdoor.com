5b6a8ec805f8cef500a920f46135b6632dbdacaa
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-11 16:53:52 +0200

    apiproxy to fix http to https error

---
+++ netlify/functions/apiProxy.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const { path } = event;
+
+  try {
+    console.log(path);
+    const response = await axios({
+      method: "GET",
+      url: `http://16.170.141.178:3001${path}`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
