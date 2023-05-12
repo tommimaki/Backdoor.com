100e8f9997892c6ba600a587403d491ae0915f54
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-12 12:49:45 +0200

    proxy fix

--- netlify/functions/apiProxy.js
+++ netlify/functions/apiProxy.js
@@ -1,34 +1,3 @@
-// const axios = require("axios");
-
-// exports.handler = async function (event, context) {
-//   let { path } = event;
-
-//   path = path.replace("/.netlify/functions/apiProxy", "");
-
-//   try {
-//     console.log(path);
-//     console.log(`http://16.170.141.178:3001/api/${path}`);
-//     const response = await axios({
-//       method: "GET",
-//       url: `http://16.170.141.178:3001/api/${path}`,
-//     });
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       },
-//       body: JSON.stringify(response.data),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify(error),
-//     };
-//   }
-// };
-
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
@@ -36,22 +5,14 @@ exports.handler = async function (event, context) {
 
   path = path.replace("/.netlify/functions/apiProxy", "");
 
-  const config = {
-    method: httpMethod,
-    url: `http://16.170.141.178:3001/api/${path}`,
-  };
-
-  if (httpMethod === "POST" || httpMethod === "PUT") {
-    config.data = JSON.parse(body);
-    config.headers = {
-      "Content-Type": "application/json",
-    };
-  }
-
   try {
     console.log(path);
     console.log(`http://16.170.141.178:3001/api/${path}`);
-    const response = await axios(config);
+    const response = await axios({
+      method: httpMethod,
+      url: `http://16.170.141.178:3001/api/${path}`,
+      data: body ? JSON.parse(body) : {},
+    });
 
     return {
       statusCode: 200,
